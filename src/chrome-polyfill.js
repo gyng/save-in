1c184556bc91e31b7ed7c7c833da331578a742bd
Ng Guoyou <ng.guoyou@gmail.com>
2017-11-29 10:45:47 +0800

    Use a promisify function for the Chrome polyfill

--- src/chrome-polyfill.js
+++ src/chrome-polyfill.js
@@ -1,16 +1,26 @@
+// Chrome somehow lazily loads and wraps func
+// so function.length cannot be used to determine arity
+const promisify = (func, arity) => args => {
+  if (arity === 0) {
+    return new Promise(resolve => {
+      func(resolve);
+    });
+  } else if (arity === 1) {
+    return new Promise(resolve => {
+      func(args, resolve);
+    });
+  } else {
+    return new Promise(resolve => {
+      func(...args, resolve);
+    });
+  }
+};
+
 if (typeof browser === "undefined") {
   if (chrome) {
     browser = chrome; // eslint-disable-line
-    const storageCbGet = browser.storage.local.get;
-    browser.storage.local.get = keys =>
-      new Promise(resolve => storageCbGet(keys, resolve));
-
-    const storageCbClear = browser.storage.local.clear;
-    browser.storage.local.clear = () =>
-      new Promise(resolve => storageCbClear(resolve));
-
-    const tabCbGet = browser.tabs.get;
-    browser.tabs.get = tabId =>
-      new Promise(resolve => tabCbGet(tabId, resolve));
+    browser.storage.local.get = promisify(browser.storage.local.get, 1);
+    browser.storage.local.clear = promisify(browser.storage.local.clear, 0);
+    browser.tabs.get = promisify(browser.tabs.get, 1);
   }
 }
