e7707b0aee34d187adc700e15ef60774d0000786
Ng Guoyou <ng.guoyou@gmail.com>
2017-12-26 14:51:58 +0800

    Declare functions for content script only after receiving OPTIONS response from background

--- src/content/content.js
+++ src/content/content.js
@@ -1,49 +1,49 @@
-const isActive = options => options.contentClickToSave;
-
-const setupKeyboardListeners = (shortcutOptions = { combo: [18] }) => {
-  const active = {};
-
-  const isComboActive = (combo, activeKeys) =>
-    combo.map(code => activeKeys[code]).every(code => code === true);
-
-  window.addEventListener("keydown", e => {
-    active[e.keyCode] = true;
-  });
-
-  window.addEventListener("keyup", e => {
-    active[e.keyCode] = false;
-  });
-
-  window.addEventListener("click", e => {
-    if (!e.target) {
-      return;
-    }
-
-    if (isComboActive(shortcutOptions.combo, active)) {
-      e.preventDefault();
-      const source = e.target.currentSrc || e.target.src;
-
-      if (source) {
-        chrome.runtime.sendMessage({
-          type: "DOWNLOAD",
-          body: {
-            url: source,
-            info: { pageUrl: `${window.location}`, srcUrl: source }
-          }
-        });
-      }
-    }
-  });
-};
-
 chrome.runtime.sendMessage(
   {
     type: "OPTIONS"
   },
   response => {
+    const isActive = options => options.contentClickToSave;
+
     if (!response || !response.body || !isActive(response.body)) {
       return;
     }
+
+    const setupKeyboardListeners = (shortcutOptions = { combo: [18] }) => {
+      const active = {};
+
+      const isComboActive = (combo, activeKeys) =>
+        combo.map(code => activeKeys[code]).every(code => code === true);
+
+      window.addEventListener("keydown", e => {
+        active[e.keyCode] = true;
+      });
+
+      window.addEventListener("keyup", e => {
+        active[e.keyCode] = false;
+      });
+
+      window.addEventListener("click", e => {
+        if (!e.target) {
+          return;
+        }
+
+        if (isComboActive(shortcutOptions.combo, active)) {
+          e.preventDefault();
+          const source = e.target.currentSrc || e.target.src;
+
+          if (source) {
+            chrome.runtime.sendMessage({
+              type: "DOWNLOAD",
+              body: {
+                url: source,
+                info: { pageUrl: `${window.location}`, srcUrl: source }
+              }
+            });
+          }
+        }
+      });
+    };
 
     setupKeyboardListeners({
       combo: [].concat(response.body.contentClickToSaveCombo)
