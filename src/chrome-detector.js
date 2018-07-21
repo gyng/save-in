845514c879c6501eb5c4e6b6eee684d10c1fb7b1
Ng Guoyou <fauxface+github@gmail.com>
2018-07-21 22:15:24 +0800

    Fix browser detection thinking it's Chrome all the time

--- src/chrome-detector.js
+++ src/chrome-detector.js
@@ -3,10 +3,20 @@ const BROWSERS = {
   FIREFOX: "FIREFOX"
 };
 
-if (typeof browser === "undefined" || browser !== chrome) {
+if (typeof browser === "undefined") {
   if (chrome) {
     CURRENT_BROWSER = BROWSERS.CHROME; // eslint-disable-line
-  } else {
-    CURRENT_BROWSER = BROWSERS.FIREFOX; // eslint-disable-line
   }
+} else {
+  if (browser.runtime.getBrowserInfo) {
+    browser.runtime.getBrowserInfo().then(res => {
+      if (res.name === "Firefox") {
+        CURRENT_BROWSER = BROWSERS.FIREFOX; // eslint-disable-line
+      }
+    });
+  }
+
+  // If we don't have browser.runtime.getBrowserInfo, assume it's Chrome
+  // Big assumption, but browser.runtime.getBrowserInfo is not well supported
+  CURRENT_BROWSER = BROWSERS.CHROME; // eslint-disable-line
 }
